98caa4e27187d297720cf65db6b945fd062c9f73
Chahit <uppalchahit@gmail.com>
2024-01-22 20:48:14 -0500

    wrote calibration code for motor encoders

---
+++ bot_client/elec/motor_calibration.py
@@ -0,0 +1,47 @@
+from motor_parameters import *
+from motor_control import control_motor_speed
+
+# In this file we will be checking motor calibration
+# we will have two
+check_dc = True
+check_encoder = True
+# 1. dc motor is connected properly  
+if check_dc:
+    print("0.5s movement in each direction")
+    motorN.forward()
+    sleep(0.5)
+    motorN.stop()
+
+    motorS.forward()
+    sleep(0.5)
+    motorS.stop()
+
+    motorE.forward()
+    sleep(0.5)
+    motorE.stop()
+
+    motorW.forward()
+    sleep(1)
+    motorW.stop()
+    print("dc test is complete")
+
+# 2. encoders are connected and working properly
+if check_encoder:
+    print("encoder test is starting")
+    encoderN.steps = 0
+    encoderE.steps = 0
+    encoderW.steps = 0
+    encoderS.steps = 0
+    print("Moving north 1 block using E encoder")
+    while abs(encoderE.steps) < steps_per_block:
+        control_motor_speed("N", 1.0)
+    print("Moving south 1 block using W encoder")
+    while abs(encoderW.steps) < steps_per_block:
+        control_motor_speed("S", 1.0)
+    print("Moving east 1 block using N encoder")
+    while abs(encoderN.steps) < steps_per_block:
+        control_motor_speed("E", 1.0)
+    print("Moving west 1 block using S encoder")
+    while abs(encoderS.steps) < steps_per_block:
+        control_motor_speed("W", 1.0)
+    print("encoder test is complete")
--- bot_client/elec/motor_control.py
+++ bot_client/elec/motor_control.py
@@ -1,23 +1,4 @@
-from gpiozero import Motor, RotaryEncoder
-from time import sleep
-
-# Motor driver connections
-motorN = Motor(forward=17, backward=27)
-motorE = Motor(forward=10, backward=9)
-motorS = Motor(forward=0, backward=5)
-motorW = Motor(forward=13, backward=19)
-
-# Quadrature encoder connections
-encoderN = RotaryEncoder(14, 15, max_steps=2500, wrap=True)
-encoderE = RotaryEncoder(23, 24, max_steps=2500, wrap=True)
-encoderS = RotaryEncoder(1, 12, max_steps=2500, wrap=True)
-encoderW = RotaryEncoder(16, 20, max_steps=2500, wrap=True)
-
-wheel_diameter = 1.850394
-wheel_circumf = 3.141593 * wheel_diameter
-steps_per_rev = 90
-steps_per_block = (7 * steps_per_rev) / wheel_circumf
-
+from motor_parameters import *
 
 def control_motor_speed(direction, speed):
     if direction == "N":
