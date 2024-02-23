2686342061ca4e9040f3185f3c4713dc742947f9
Hitarth Desai <hitarthdesai306@gmail.com>
2024-02-23 18:55:06 -0500

    remove code we don't need for now

--- bot_client/decisionModule.py
+++ bot_client/decisionModule.py
@@ -1,15 +1,10 @@
 import asyncio
-import heapq
 from typing import List, Tuple
-from algo import Node
 from heuristic import Heuristic
-from util import location_to_direction, direction_to_elec_move, next_move_in_direction
+from util import location_to_direction
 
 from gameState import GameState, Directions, Location, GameModes
-import sys
 
-if "-elec" in sys.argv:
-    from motor_control import move_robot
 
 GRID_WIDTH = 31
 GRID_HEIGHT = 31
@@ -40,143 +35,13 @@ class DecisionModule:
         self.state = state
         self.heuristic = Heuristic(state)
 
-    # TODO: Consider chase vs scatter mode.
-    def _get_target(self) -> Location:
-        try:
-            return self._find_closest_safe_pellet()
-        except Exception as e:
-            print(f"Error in finding closest pellet: {e}")
-            return self.state.pacmanLoc
-
-    def _is_pellet_safe(
-        self, pellet: Location, distance_threshold=DISTANCE_THRESHOLD
-    ) -> bool:
-        if type(pellet) is not Location:
-            pelletLoc = Location(self.state)
-            pelletLoc.update((pellet[0] << 8) | pellet[1])
-            pellet = pelletLoc
-
-        normal_ghosts = [g for g in self.state.ghosts if not g.isFrightened()]
-        return all(
-            pellet.distance_to(g.location) > distance_threshold for g in normal_ghosts
-        )
-
-    def _find_closest_safe_pellet(self) -> Location:
-        """
-        Find the closest pellet to the current location of Pacman
-        It skips any pellets that are within a CONSTANT distance of any normal ghost
-
-        If no pellets are found, it tries to run away from all ghosts
-        """
-
-        safe_pellets = [
-            (x, y)
-            for x in range(GRID_WIDTH)
-            for y in range(GRID_HEIGHT)
-            if self.state.pelletAt(x, y) and self._is_pellet_safe((x, y))
-        ]
-
-        try:
-            x, y = min(
-                safe_pellets,
-                key=lambda point: self.state.pacmanLoc.distance_to_overload(point)
-                - self.heuristic._cluster_heuristic(point),
-            )
-
-            closest_safe_pellet = Location(self.state)
-            closest_safe_pellet.update((x << 8) | y)
-            return closest_safe_pellet
-
-        except Exception as e:
-            print(f"Error in _find_closest_safe_pellet: {e}")
-            return self._get_away_from_ghosts_when_cant_find_pellets()
-
-    def _get_away_from_ghosts_when_cant_find_pellets(self):
-        print("No pellets found, running away from ghosts")
-        ghost_plans = [
-            next_move_in_direction(g.location, g.guessPlan()) for g in self.state.ghosts
-        ]
-
-        all_possible_moves = map(
-            lambda d: next_move_in_direction(self.state.pacmanLoc, d), ALL_DIRECTIONS
-        )
-        # Remove any moves that hit a wall or a ghost is planning to move to
-        valid_moves = filter(
-            lambda loc: not self.state.wallAt(loc.row, loc.col)
-            and loc not in ghost_plans,
-            all_possible_moves,
-        )
-
-        # If there are no possible moves, stay in the current location
-        if not valid_moves:
-            return self.state.pacmanLoc
-
-        normal_ghosts = [g for g in self.state.ghosts if not g.isFrightened()]
-        best_move = max(
-            valid_moves,
-            key=lambda move: min(move.distance_to(g.location) for g in normal_ghosts),
-        )
-
-        return best_move
+    def _algo(self, start: Location) -> List[Location]:
+        return start
 
     def _get_next_move(self) -> Directions:
-        target = self._get_target()
         start = self.state.pacmanLoc
-        path = self._algo(start, target)
-        if path is not None and len(path) > 0:
-            _move = path[0]
-            move = location_to_direction(start, _move)
-            return move
-
-        print("No path found")
-        return Directions.NONE
-
-    def _algo(self, start: Location, target: Location) -> List[Location]:
-        open_list: List[Node] = list()
-        closed_set = set()
-        head = Node(start, None)
-        head.g = 0
-        head.h = self.heuristic.get_overall_heuristic(start, target)
-        head.f = head.h
-        heapq.heappush(open_list, head)
-
-        while open_list:
-            curr = heapq.heappop(open_list)
-            if curr.position.at(target.row, target.col):
-                path: List[Location] = []
-                while curr.position != start:
-                    try:
-                        path.append(curr.position)
-                        curr = curr.parent
-                    except Exception as e:
-                        print(f"Error: {e}")
-                        break
-                path.reverse()
-                return path
-
-            closed_set.add(Location.__str__(curr.position))
-            all_neighbors = map(
-                lambda dir: (curr.position.row + dir[0], curr.position.col + dir[1]),
-                [(1, 0), (-1, 0), (0, 1), (0, -1)],
-            )
-
-            valid_neighbors = filter(
-                lambda pos: not self.state.wallAt(pos[0], pos[1]),
-                all_neighbors,
-            )
-
-            for neighbor in valid_neighbors:
-                if str(neighbor) in closed_set:
-                    continue
-                loc = Location(self.state)
-                loc.update((neighbor[0] << 8) | neighbor[1])
-                node = Node(loc, curr)
-                node.g = curr.g + 1
-                node.h = self.heuristic.get_overall_heuristic(loc, target)
-                node.f = node.g + node.h
-                heapq.heappush(open_list, node)
-
-        return None
+        move = self._algo(start)
+        return location_to_direction(start, move)
 
     async def decisionLoop(self) -> None:
         while self.state.isConnected():
@@ -189,10 +54,6 @@ class DecisionModule:
 
             self.state.lock()
             next_move = self._get_next_move()
-            elec_move = direction_to_elec_move(next_move)
-
-            if "-elec" in sys.argv:
-                move_robot(1, elec_move)
 
             self.state.queueAction(4, next_move)
             self.state.unlock()
--- bot_client/util/locationToDirection.py
+++ bot_client/util/locationToDirection.py
@@ -4,15 +4,21 @@ from gameState import Location, Directions
 
 def location_to_direction(p: Location, n: Location) -> Directions:
     try:
+        if p.row == n.row and p.col == n.col:
+            return Directions.NONE
+
         if p.row == n.row:
             if p.col < n.col:
                 return Directions.RIGHT
             else:
                 return Directions.LEFT
-        else:
+
+        if p.col == n.col:
             if p.row < n.row:
                 return Directions.DOWN
             else:
                 return Directions.UP
+
+        return Directions.NONE
     except:
         return Directions.NONE
